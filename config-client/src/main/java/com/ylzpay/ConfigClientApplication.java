15a3eaf7a29e8112d8d7e47c7cadcda55921523f
xuee <17865429267@163.com>
2019-07-26 19:24:21 +0800

    www

---
+++ config-client/src/main/java/com/ylzpay/ConfigClientApplication.java
@@ -0,0 +1,21 @@
+package com.ylzpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
